577cb1c7640c0d63b3d84d5401a1da73a7dee987
Satyam Kumar <isatyamks@gmail.com>
2024-09-13 20:06:56 +0530

    chore: recommendation model is added

---
+++ recommendation_system/app.py
@@ -0,0 +1,10 @@
+import pickle
+
+movies = pickle.load(open('recommendation_system/model/movie_list.pkl', 'rb'))
+similarity = pickle.load(open('recommendation_system/model/similarity.pkl', 'rb'))
+
+print("Movies List:")
+print(movies[title])
+
+print("\nSimilarity Matrix:")
+print(similarity)
