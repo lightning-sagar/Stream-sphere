760b469fa9b4d7e7106cf5091f604fd64f6c6c58
Lightning Sagar <lightningsagar0@gmail.com>
2024-10-04 16:23:19 +0530

    70% done

---
+++ install_and_run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+npm install
+# Navigate to the recommendation_system directory
+cd recommendation_system
+
+# Install Python packages
+pip install uvicorn numpy pandas
+
+# Go back to the root directory
+cd ..
+
+# Run nodemon and uvicorn concurrently
+concurrently "nodemon app.js" "cd recommendation_system && uvicorn new:app --reload"
